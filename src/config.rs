c558805c3e4880e75c0dd48bd685daf2620b87d8
WangYan <me@wangyan.org>
2025-08-02 18:45:51 +0800

    update servers and pub_key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["139.199.193.177"];
+pub const RS_PUB_KEY: &str = "0zc2OpRQ3AnEI6p2ThleSnGNwcIPVeea5aQ9uU+Ojng=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
